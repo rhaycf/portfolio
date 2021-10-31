06df9c3cdb39f600bdf453fc9a7f74ac7c5e308f
Rhayane Fabres <rhayanefabres@gmail.com>
2021-10-31 15:19:15 -0300

    :package: menu sidebar

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ O controle das tarefas desse projeto será realizado no GitHub.
 
 ## Icones
 
-:package: Nova funcionalidade
-:up: Atualização
-:beatle: Correção de bug
-:checkred_flag: Release
+- :package: Nova funcionalidade
+- :up: Atualização
+- :beetle: Correção de bug
+- :checkered_flag: Release
---
+++ script.js
@@ -0,0 +1,10 @@
+
+document.querySelector(".hamburguer").addEventListener("click", () =>
+    document.querySelector(".container").classList.toggle("show-menu")
+);
+
+/* let hamburguer = document.querySelector(".hamburguer");
+
+hamburguer.addEventListener("click", function() {
+    document.querySelector(".container").classList.toggle("show-menu");
+}); */
